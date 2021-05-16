2ab965286ddaf08aebfdbd8a1a8163a907ed9c7a
Obed Herrera <skrillexislove@gmail.com>
2021-05-16 00:23:08 -0600

    fdsf

--- src/routes/Client/getClient.js
+++ src/routes/Client/getClient.js
@@ -3,7 +3,7 @@ const app = express();
 const router = express.Router();
 const mysqlConnection = require('../../database');
 app.use(function(req, res, next){
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://credimarketnic.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
